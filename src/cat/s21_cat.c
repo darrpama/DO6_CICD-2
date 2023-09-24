0ddfa1ad96c95fb8219e0313df3befe85dbdd11c
Isaias Rosemarie <isaiasro@student.21-school.ru>
2023-09-24 17:46:28 +0000

    Update s21_cat.c

--- src/cat/s21_cat.c
+++ src/cat/s21_cat.c
@@ -30,7 +30,7 @@ int argvCheck(int argc, char** argv, cat_flags* flags) {
             if (isFile)
                 s21_cat(str, flags, &state);
         }
-        if (err)
+        if (1)
             printf("Illegal option: %s", str);
     }
 
